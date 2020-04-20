9b2758085bf1d6db2654ffbbd1354cbb12e1eb0e
Aleksi Kervinen <me@aleksi.me>
2020-04-20 20:06:30 +0300

    improve drawer interaction

--- app/src/main/java/me/aleksi/fewer/MainActivity.kt
+++ app/src/main/java/me/aleksi/fewer/MainActivity.kt
@@ -1,17 +1,23 @@
 package me.aleksi.fewer
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.os.PersistableBundle
 import android.os.ResultReceiver
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.view.GravityCompat
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import me.aleksi.fewer.fever.*
 import me.aleksi.fewer.ui.*
 import me.aleksi.fewer.ui.FeedGroup
@@ -27,27 +33,49 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
     private var activeFeed: Feed? = null
 
     private lateinit var feedItemList: RecyclerView
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        feedItemList = findViewById(R.id.feed)
+        feedItemList = feed
         feedItemList.adapter = feedItemAdapter
 
         feedAdapter.onFeedClick = {
             Log.d("TAG", "Feed clicked: ${it.title}")
             activeFeed = it
             filterFeedItems(activeFeed)
+
+            drawer_layout.closeDrawer(GravityCompat.START)
         }
 
-        val recycler = findViewById<RecyclerView>(R.id.drawer_nav)
-        recycler.adapter = feedAdapter
+        drawer_nav.adapter = feedAdapter
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            R.string.feedList_open,
+            R.string.feedList_close
+        )
+        drawer_layout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
 
         refreshFeedList()
         refresh()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        actionBarDrawerToggle.onConfigurationChanged(newConfig)
     }
 
     private fun refresh() {
@@ -89,6 +117,10 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
         when (item.itemId) {
             R.id.settingsAction -> {
                 startActivity(Intent(this, SettingsActivity::class.java))
@@ -99,6 +131,14 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onItemClicked(item: FeedItem) {
